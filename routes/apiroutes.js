6f7134937ed6efd3d5b793c65dfc1503761036b4
Sami Fares <djsamif@gmail.com>
2022-03-19 13:02:52 -0400

    added IDs

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,22 +1,29 @@
 const router = require(`express`).Router();
 const fs = require(`fs`)
 const util = require('util');
+const { v4: uuidv4 } = require(`uuid`);
 
 const readFromFile = util.promisify(fs.readFile);
 
 
 
 router.get('/notes', (req, res) =>
-  {console.log("get")
-  readFromFile(`./db/db.json`, `utf8`).then(data => {res.json(JSON.parse(data))})
+  {readFromFile(`./db/db.json`, `utf8`).then(data => {res.json(JSON.parse(data))})
 }
 );
 
 router.post('/notes', (req, res) =>
-  {console.log(req.body)
+  {
+      const { title, text } = req.body
+      const note = {
+          title,
+          text,
+          id: uuidv4(),
+      }
+      
     readFromFile(`./db/db.json`, `utf8`).then(data => {
         const parsedData = JSON.parse(data);
-        parsedData.push(req.body);
+        parsedData.push(note);
         fs.writeFile(`./db/db.json`, JSON.stringify(parsedData, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written`)
   );
